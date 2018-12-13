ee9c2ed28461494dc4c9c5ceb14d057d67537d22
dtpmilke <32977534+dtpmilke@users.noreply.github.com>
2018-12-13 23:58:51 +0400

    upd

--- unics/js/common.js
+++ unics/js/common.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $('#clock').countdown("2018/12/14", function(event) {
+    $('#clock').countdown("2018/12/18", function(event) {
         var totalHours = event.offset.totalDays * 24 + event.offset.hours;
         $(this).html(event.strftime('<div class="item">' + totalHours + '<span>Часов</span></div><div class="divider">:</div><div class="item">%M<span>Минут</span></div><div class="divider">:</div><div class="item">%S<span>Секунд</span></div>'));
     });
@@ -13,31 +13,62 @@ $(function() {
             spaceBetween: 20,
             freeMode: false,
             grabCursor: true,
+            allowTouchMove:false,
             navigation: {
-                nextEl: '.slider-buttons-next',
-                prevEl: '.slider-buttons-prev',
+                nextEl: '#partner-slider .slider-buttons-next',
+                prevEl: '#partner-slider .slider-buttons-prev',
             },
             breakpoints: {
                 1024: {
                     slidesPerView: 3,
                     spaceBetween: 16,
                 }
-                
+
             },
             on: {
                 reachEnd: function() {
-                    $('.slider-buttons-next').hide();
-                    $('.slider-buttons-prev').show();
+                    $('#partner-slider .slider-buttons-next').hide();
+                    $('#partner-slider .slider-buttons-prev').show();
                 },
                 reachBeginning: function() {
-                    $('.slider-buttons-prev').hide();
-                    $('.slider-buttons-next').show();
+                    $('#partner-slider .slider-buttons-prev').hide();
+                    $('#partner-slider .slider-buttons-next').show();
                 }
             }
-
         })
+    }
 
+     if ($('#roadmap_slider').length) {
+        var roadMapSlider = new Swiper('#roadmap_slider', {
+            // Optional parameters
+            loop: false,
+            slidesPerView: 4,
+            spaceBetween: 25,
+            freeMode: false,
+            grabCursor: false,
+            allowTouchMove:false,
+            navigation: {
+                nextEl: '#roadmap_slider .slider-buttons-next',
+                prevEl: '#roadmap_slider .slider-buttons-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 16,
+                }
 
+            },
+            // on: {
+            //     reachEnd: function() {
+            //         $('.slider-buttons-next').hide();
+            //         $('.slider-buttons-prev').show();
+            //     },
+            //     reachBeginning: function() {
+            //         $('.slider-buttons-prev').hide();
+            //         $('.slider-buttons-next').show();
+            //     }
+            // }
+        })
     }
 
 
