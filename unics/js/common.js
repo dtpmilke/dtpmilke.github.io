b2d2a57cd76a2804dbddfb7384327cae4431c24f
dtpmilke <32977534+dtpmilke@users.noreply.github.com>
2018-12-18 21:23:56 +0400

    Final v1

--- unics/js/common.js
+++ unics/js/common.js
@@ -1,10 +1,66 @@
 $(function() {
+	contentEffect()
+    //Функция переключения меню
+    // показ/скрытие меню
+    function openMenu() {
+        saveScrollTop();
+        $('html').addClass('open-menu');
+    }
 
-    $('#clock').countdown("2018/12/18", function(event) {
+    function closeMenu() {
+        $('html').removeClass('open-menu');
+        setScrollTop();
+    }
+
+    // для модальных окон на мобильных устройствах
+    // восстановление значения скролла после закрытия мод.окон
+    // запоминаем текущую позицию скролла
+    function saveScrollTop() {
+        if ($(window).width() * 1 <= 1000) {
+            if ($(window).scrollTop() > 0) {
+                $('body').attr('data-scrolltop', $(window).scrollTop());
+            }
+        }
+    }
+    // установка позиции скролла
+    function setScrollTop() {
+        if ($(window).width() * 1 <= 1000) {
+            if ($('body').attr('data-scrolltop') * 1 > 0) {
+                $(window).scrollTop($('body').attr('data-scrolltop'));
+            }
+        }
+    }
+    //Переключение моб меню
+    $('#menu_button').on('click', function() {
+        if ($('html').hasClass('open-menu')) {
+            closeMenu();
+
+        } else {
+            openMenu();
+        }
+
+        return false;
+    });
+
+    //Часы
+    $('#clock').countdown("2018/12/20", function(event) {
         var totalHours = event.offset.totalDays * 24 + event.offset.hours;
         $(this).html(event.strftime('<div class="item">' + totalHours + '<span>Часов</span></div><div class="divider">:</div><div class="item">%M<span>Минут</span></div><div class="divider">:</div><div class="item">%S<span>Секунд</span></div>'));
     });
 
+    //Переключение FAQ
+    if ('.faq'.length) {
+        $('.items .item .arrow').on('click', function() {
+            var itemBlock = $(this).parent().parent();
+            if (itemBlock.hasClass('closed')) {
+                itemBlock.removeClass('closed').addClass('opened');
+            } else {
+                itemBlock.removeClass('opened').addClass('closed');
+            }
+        });
+    }
+
+    //Слайдер партнеры
     if ($('#partner-slider').length) {
         var mySwiper = new Swiper('#partner-slider', {
             // Optional parameters
@@ -13,7 +69,7 @@ $(function() {
             spaceBetween: 20,
             freeMode: false,
             grabCursor: true,
-            allowTouchMove:false,
+            allowTouchMove: false,
             navigation: {
                 nextEl: '#partner-slider .slider-buttons-next',
                 prevEl: '#partner-slider .slider-buttons-prev',
@@ -22,6 +78,10 @@ $(function() {
                 1024: {
                     slidesPerView: 3,
                     spaceBetween: 16,
+                },
+                575: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
                 }
 
             },
@@ -37,16 +97,15 @@ $(function() {
             }
         })
     }
-
-     if ($('#roadmap_slider').length) {
+    //Слайдер дорожной карты
+    if ($('#roadmap_slider').length) {
         var roadMapSlider = new Swiper('#roadmap_slider', {
-            // Optional parameters
             loop: false,
             slidesPerView: 4,
             spaceBetween: 25,
             freeMode: false,
             grabCursor: false,
-            allowTouchMove:false,
+            allowTouchMove: false,
             navigation: {
                 nextEl: '#roadmap_slider .slider-buttons-next',
                 prevEl: '#roadmap_slider .slider-buttons-prev',
@@ -55,22 +114,159 @@ $(function() {
                 1024: {
                     slidesPerView: 3,
                     spaceBetween: 16,
+                },
+                575: {
+                    direction: 'vertical',
+                    slidesPerView: 3,
+                    spaceBetween: 5,
                 }
 
             },
-            // on: {
-            //     reachEnd: function() {
-            //         $('.slider-buttons-next').hide();
-            //         $('.slider-buttons-prev').show();
-            //     },
-            //     reachBeginning: function() {
-            //         $('.slider-buttons-prev').hide();
-            //         $('.slider-buttons-next').show();
-            //     }
-            // }
         })
     }
+    //Форма в футере
+    if ($('#footer_form_2').length) {
+        function initFooterForm() {
+            var $form = $('#footer_form_2');
+            $form.on('change', '.validate', function() {
+                validateField($(this));
+            });
 
+            var $success = $form.find('.success-form');
+
+            $('#footer_submit').on('click', function() {
+                console.log(1);
+                if (validateForm($form)) {
+                    console.log(2);
+                    var data = $form.serialize();
+                    $.ajax({
+                        url: '#',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: data,
+                        success: function(data) {
+                            successForm();
+                        },
+                        error: function(data) {
+                            successForm();
+                        },
+                        timeout: 60000
+                    });
+                }
+                return false;
+            });
+
+
+            function successForm() {
+                console.log(3);
+                $success.show('fade');
+
+                setTimeout(function() {
+                    clearForm($form);
+                    $success.hide('fade');
+                }, 3000);
+            }
+        }
+        initFooterForm();
+
+    }
+    //Форма в контенте
+    if ($('#email_form_1').length) {
+
+
+        function initFeedbackForm() {
+            var $form = $('#email_form_1');
+            $form.on('change', '.validate', function() {
+                validateField($(this));
+            });
+
+            var $success = $form.find('.success-form');
+
+            $('#email_submit').on('click', function() {
+                if (validateForm($form)) {
+                    var data = $form.serialize();
+                    $.ajax({
+                        url: '#',
+                        type: 'POST',
+                        dataType: 'json',
+                        data: data,
+                        success: function(data) {
+                            successForm();
+                        },
+                        error: function(data) {
+                            successForm();
+                        },
+                        timeout: 60000
+                    });
+                }
+                return false;
+            });
+
+
+            function successForm() {
+                $success.show('fade');
+
+                setTimeout(function() {
+                    clearForm($form);
+                    $success.hide('fade');
+                }, 3000);
+            }
+        }
+        initFeedbackForm();
+
+    }
+    //Функция для плейсхолдера из form.js
+    initPlaceholders();
+
+    var timeoutVideo;
+    $('.video').on('click', function() {
+
+        clearTimeout(timeoutVideo);
+        var youtubeID = $(this).attr('data-youtube');
+        var frameHTML = '';
+        if (youtubeID != undefined && youtubeID != '') {
+
+            frameHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/' + youtubeID + '?border=0&showinfo=0&showsearch=0&autoplay=1" frameborder="0" allowfullscreen></iframe>';
+        }
+
+        if (frameHTML != '') {
+            timeoutVideo = setTimeout(function() {
+                $('#modal_video_frame').html(frameHTML);
+            }, 500);
+            $('#modal_video').addClass('show');
+            $('html').addClass('show-video');
+            saveScrollTop();
+        }
+
+        return false;
+    });
+
+    $('#close_video').on('click', function() {
+        clearTimeout(timeoutVideo);
+        $('#modal_video').removeClass('show');
+        $('#modal_video_frame').html('');
+        $('html').removeClass('show-video');
+        setScrollTop();
+        return false;
+    });
+
+
+    /*контентные эффекты*/
+    function contentEffect() {
+        var offset = 100;
+        if ($('html').hasClass('touch')) {
+            offset = 0;
+        }
+
+        //обертка для эффектов
+        if ($('.contentEffect').length) {
+            $('.contentEffect').viewportChecker({
+                classToAdd: 'effectShow',
+                offset: offset,
+                callbackFunction: function(elem, action) {}
+            });
+        }
+    }
 
 
 
