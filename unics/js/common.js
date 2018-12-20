f1f319b597b38b4fb3d6f8735e65fa7df918a95c
dtpmilke <32977534+dtpmilke@users.noreply.github.com>
2018-12-20 11:18:23 +0400

    FF

--- unics/js/common.js
+++ unics/js/common.js
@@ -43,7 +43,7 @@ $(function() {
     });
 
     //Часы
-    $('#clock').countdown("2018/12/20", function(event) {
+    $('#clock').countdown("2018/12/22", function(event) {
         var totalHours = event.offset.totalDays * 24 + event.offset.hours;
         $(this).html(event.strftime('<div class="item">' + totalHours + '<span>Часов</span></div><div class="divider">:</div><div class="item">%M<span>Минут</span></div><div class="divider">:</div><div class="item">%S<span>Секунд</span></div>'));
     });
